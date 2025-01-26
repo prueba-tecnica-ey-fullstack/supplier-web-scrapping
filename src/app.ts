699e3351b938a65c6ba233dd5ee9f91eda746b2e
renato-av <renato.avela.17@gmail.com>
2025-01-26 07:59:26 -0500

    feat: add express-rate-limit middleware to limit request rate

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import cron from 'node-cron'
+import rateLimit from 'express-rate-limit';
 
 import routes from './routes/scrapping.routes'
 import { verifyApiKey } from './middlewares/auth.middleware'
@@ -10,8 +11,14 @@ import { executeScrapping } from './tasks/scrapping.task'
 const BASE_API_URL = '/api'
 const app = express()
 
-// TODO: add explicit origin list
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 20,
+  message: 'Demasiadas solicitudes desde esta IP. Por favor, intenta de nuevo en unos minutos',
+});
+
 app.use(cors())
+app.use(limiter)
 app.use(morgan('dev'))
 
 app.use(`${BASE_API_URL}`, verifyApiKey, routes)
